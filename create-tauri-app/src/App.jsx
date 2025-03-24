7c1d1931763301986094347cedb6d815535d5597
Eduardo Lopes <edulopess@rede.ulbra.br>
2025-03-24 14:30:37 -0300

    inicio

---
+++ create-tauri-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <p>SDKASDKASDK</p>
+ 
+  )   
+}
+
+export default App;
